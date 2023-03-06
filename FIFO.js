47f38e06b2455355f244633590aa12647b3e346f
Emmanuel Ochoa <eorivera95@gmail.com>
2023-03-06 09:46:00 -0600

    simple FIFO and LIFO

---
+++ FIFO.js
@@ -0,0 +1,12 @@
+// First In First Out
+
+const stack = []
+
+stack.push('A')
+stack.push('B')
+stack.push('C')
+
+const firstIn = stack.shift()
+
+console.log(firstIn)
+console.log(stack)
---
+++ LIFO.js
@@ -0,0 +1,11 @@
+// Last In First Out
+const stack = []
+
+stack.push('A')
+stack.push('B')
+stack.push('C')
+
+const lastIn = stack.pop()
+
+console.log(lastIn)
+console.log(stack)
